f5b1d80c7c403240bb6e9db653ff2d1f8c84baa9
tenderness330 <tenderness330@yahoo.co.jp>
2020-02-12 17:53:18 +0900

    date

--- config.fish
+++ config.fish
@@ -2,9 +2,18 @@ fish_vi_key_bindings
 
 function fish_user_key_bindings
   bind \cP 'accept-autosuggestion' -M insert
+  bind \cN 'accept-autosuggestion' -M insert
 end
+
+function fish_prompt
+  printf '[%s]%s %s%s > ' (date +"%m/%d %H:%M") (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+end  
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '$HOME/google-cloud-sdk/path.fish.inc' ]; . '$HOME/google-cloud-sdk/path.fish.inc'; end
 
+umask 022
 set PATH $HOME/.local/bin /usr/local/go/bin $HOME/go/bin $PATH
+
+alias vi='vim'
+alias aws='/usr/local/bin/aws'
